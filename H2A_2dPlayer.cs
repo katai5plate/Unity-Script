b4563b3ceb242eb0f0e3d4464bfb7eab4ada2802
katai5plate <katai5plate.sub@gmail.com>
2017-09-18 23:53:07 +0900

    init

---
+++ H2A_2dPlayer.cs
@@ -0,0 +1,163 @@
+﻿/********************************************************************************
+	
+	H2A_2dPlayer
+
+	用途：
+	ツクール風の2D移動を再現するスクリプト。
+	H2A_CharaChipがADDされたオブジェクトに一緒に付けることで、
+	ツクール風の歩行アニメーションを実現することができる。（勿論無くても動作する。）
+
+	前提：
+	このスクリプトがADDされたオブジェクトの子供オブジェクトには、
+	SpriteRendererがADDされていなければならない。
+
+	最終更新：2017/09/18
+
+ ********************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 移動に関するパラメーター
+/// </summary>
+[System.Serializable]
+public class WalkProcess{
+	//１歩の長さ
+	public float moveGrid = 1.0f;
+	//４方向の方角
+	public Vector3 up = new Vector3(0,1,0);
+	public Vector3 down = new Vector3(0,-1,0);
+	public Vector3 left = new Vector3(-1,0,0);
+	public Vector3 right = new Vector3(1,0,0);
+	//移動スピード
+	public float moveSpeed = 0.05f;
+	//デフォルトの向き
+	public int direction = 2;
+}
+
+/// <summary>
+/// メインクラス
+/// </summary>
+public class H2A_2dPlayer : MonoBehaviour {
+	
+	//移動に関するパラメーター
+	public WalkProcess walk;
+
+	//座標移動アニメ用
+	float walkTime;
+	Vector3 aPos,bPos;
+
+	//足踏みアニメ用
+	int footStat=0;
+	int pfs=1;
+
+	//連携するコンポーネント
+	H2A_CharaChip h2A_CharaChip;
+	SpriteRenderer image;
+
+	//当たり判定用
+	RaycastHit2D raycast;
+
+	//雑用
+	bool k;
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start () {
+		//h2A_CharaChipとの連携
+		h2A_CharaChip = GetComponent<H2A_CharaChip>();
+		//子オブジェクトのSpriteと連携
+		image = GetComponentInChildren<SpriteRenderer>();
+	}
+
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update () {
+		//描画
+		DrawProcess();
+		//メイン
+		MainProcess();
+	}
+
+	/// <summary>
+	/// 正面を調べて当たり判定
+	/// </summary>
+	/// <param name="dir">方角</param>
+	public RaycastHit2D nextStepCast(Vector2 dir){
+		return Physics2D.BoxCast(transform.position,Vector2.one*(walk.moveGrid/2),0f,dir,walk.moveGrid);
+	}
+
+	/// <summary>
+	/// 描画プロセス
+	/// </summary>
+	void DrawProcess(){
+		if(h2A_CharaChip!=null){
+			image.sprite = h2A_CharaChip.ChoiceCharaSprite(0,walk.direction,footStat);
+		}
+	}
+	
+	/// <summary>
+	/// メインプロセス
+	/// </summary>
+	void MainProcess(){
+		//立ち止まっているなら
+		if(walkTime==0){
+			//上に移動
+			if(Input.GetAxisRaw("Vertical")==1){
+				walk.direction=8;
+				bPos=walk.up;
+				k=true;
+			}
+			//下に移動
+			if(Input.GetAxisRaw("Vertical")==-1){
+				walk.direction=2;
+				bPos=walk.down;
+				k=true;
+			}
+			//左に移動
+			if(Input.GetAxisRaw("Horizontal")==-1){
+				walk.direction=4;
+				bPos=walk.left;
+				k=true;
+			}
+			//右に移動
+			if(Input.GetAxisRaw("Horizontal")==1){
+				walk.direction=6;
+				bPos=walk.right;
+				k=true;
+			}
+			//移動キーが押されたなら
+			if(k){
+				//正面に障害物がある
+				if(!!nextStepCast(bPos)){
+					//移動キー入力は無かったことに
+					k=false;
+				//正面に障害物はない
+				}else{
+					//歩き始める
+					walkTime=walk.moveSpeed;
+					footStat=-pfs; pfs=footStat;
+					aPos=transform.position;
+					bPos=aPos+(bPos*walk.moveGrid);
+					k=false;
+				}
+			}
+		//歩いているなら
+		}else{
+			//walk.moveSpeedの早さで0から1まで到達したら一度立ち止まる
+			transform.position = Vector3.Lerp(aPos,bPos,walkTime);
+			if(walkTime>=1){
+				transform.position = bPos;
+				walkTime=0;
+				footStat=0;
+			}else{
+				if(walkTime>=0.5f)footStat=0;
+				walkTime+=walk.moveSpeed;
+			}
+		}
+	}
+}
+
---
+++ H2A_CharaChip.cs
@@ -0,0 +1,137 @@
+﻿/********************************************************************************
+	
+	H2A_CharaChip
+
+	用途：
+	RPGツクール2000、2003、VX、MVの歩行グラフィックをUnityで扱いやすくするスクリプト。
+	H2A_2dPlayerがADDされたオブジェクトに一緒に付けることで、
+	ツクール風の歩行アニメーションを実現することができる。
+
+	前提：
+	使用する歩行グラフィック素材はRTPの素材規格そのままの構成の画像を使用し、
+	MultipleSprite化して96枚の画像に分割すること。また、透過PNGにすることを推奨。
+
+	最終更新：2017/09/18
+
+ ********************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 歩行グラフィック（４方向）の設定クラス
+/// </summary>
+[System.Serializable]
+public class WalkSprite{
+	public Vector3 up = 	 new Vector3(0,0,0);
+	public Vector3 down =	 new Vector3(0,0,0);
+	public Vector3 left =	 new Vector3(0,0,0);
+	public Vector3 right = 	 new Vector3(0,0,0);
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="args">上下左右各３つのMultipleSprite番号</param>
+	public WalkSprite(params int[] args){
+		up = 	 new Vector3(args[0],args[1],args[2]);
+		down =	 new Vector3(args[3],args[4],args[5]);
+		left =	 new Vector3(args[6],args[7],args[8]);
+		right =  new Vector3(args[9],args[10],args[11]);
+	}
+}
+
+/// <summary>
+/// 歩行グラフィック配列タイプ列挙
+/// </summary>
+public enum WalkSpriteType {
+	RPG_Maker_2000_3 = 0,
+	RPG_Maker_VX_MV,
+	User
+}
+
+/// <summary>
+/// メインクラス
+/// </summary>
+public class H2A_CharaChip : MonoBehaviour {
+	
+	//Resourceフォルダ内の画像ファイルの名前（相対パス・拡張子不要）
+	public string fileName;
+
+	//歩行グラフィック配列データを選択
+	public WalkSpriteType type;
+
+	//RPGツクール2000～2003の歩行グラフィック配列データ
+	[HideInInspector]
+	public WalkSprite[] rpg2kMap = {
+		new WalkSprite(0,1,2,24,25,26,36,37,38,12,13,14),
+		new WalkSprite(3,4,5,27,28,29,39,40,41,15,16,17),
+		new WalkSprite(6,7,8,30,31,32,42,43,44,18,19,20),
+		new WalkSprite(9,10,11,33,34,35,45,46,47,21,22,23),
+		new WalkSprite(48,49,50,72,73,74,84,85,86,60,61,62),
+		new WalkSprite(51,52,53,75,76,77,87,88,89,63,64,65),
+		new WalkSprite(54,55,56,78,79,80,90,91,92,66,67,68),
+		new WalkSprite(57,58,59,81,82,83,93,94,95,69,70,71)
+	};
+
+	//RPGツクールVX～MVの歩行グラフィック配列データ
+	[HideInInspector]
+	public WalkSprite[] rpgVxMap = {
+		new WalkSprite(36,37,38,0,1,2,12,13,14,24,25,26),
+		new WalkSprite(39,40,41,3,4,5,15,16,17,27,28,29),
+		new WalkSprite(42,43,44,6,7,8,18,19,20,30,31,32),
+		new WalkSprite(45,46,47,9,10,11,21,22,23,33,34,35),
+		new WalkSprite(84,85,86,48,49,50,60,61,62,72,73,74),
+		new WalkSprite(87,88,89,51,52,53,63,64,65,75,76,77),
+		new WalkSprite(90,91,92,54,55,56,66,67,68,78,79,80),
+		new WalkSprite(93,94,95,57,58,59,69,70,71,81,82,83)
+	};
+
+	//ユーザー定義の歩行グラフィック配列データ
+	public WalkSprite[] userMap = new WalkSprite[8];
+
+	/// <summary>
+	/// MultipleSpriteから１枚抽出する
+	/// ただし画像名が「[fileName]_[num]」の形になっている場合に限る。
+	/// </summary>
+	/// <param name="num">MultipleSprite番号</param>
+	public Sprite LoadMulSprite(int num){
+		Sprite[] sprites = Resources.LoadAll<Sprite>(fileName);
+		return System.Array.Find<Sprite>( sprites, (sprite) => sprite.name.Equals(fileName+"_"+num));
+	}
+
+	/// <summary>
+	/// MultipleSpriteから条件に合った１枚を抽出する
+	/// </summary>
+	/// <param name="actor">キャラクター番号（左上から右に0123、左下から右に4567）</param>
+	/// <param name="dir">向いている方角（テンキーを参照：2468）</param>
+	/// <param name="foot">足の状態（-1/0/1）</param>
+	public Sprite ChoiceCharaSprite(int actor,int dir,int foot){
+		WalkSprite w;
+		Vector3 v;
+		float id;
+		switch(type){
+			case WalkSpriteType.RPG_Maker_2000_3:
+				w = rpg2kMap[actor];
+				break;
+			case WalkSpriteType.RPG_Maker_VX_MV:
+				w = rpgVxMap[actor];
+				break;
+			default:
+				w = userMap[actor];
+				break;
+		}
+		switch(dir){
+			case 8:	v = w.up;		break;
+			case 2:	v = w.down;		break;
+			case 4:	v = w.left;		break;
+			case 6:	v = w.right;	break;
+			default:				return null;
+		}
+		switch(foot){
+			case -1:	id = v.x;	break;
+			case 0:		id = v.y;	break;
+			case 1:		id = v.z;	break;
+			default:				return null;
+		}
+		return LoadMulSprite((int)id);
+	}
+}
